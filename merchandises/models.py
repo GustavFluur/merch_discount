c8d1cf1ba814f9a839cb958b2b51d39f7ff6dccd
GustavFluur <gustavfluur@hotmail.com>
2023-12-07 18:48:16 +0000

    Added new app into the workspace, added new content & features within merchandises app, and superuser has been created

---
+++ merchandises/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=220)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+
+class Merchandise(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    price = models.FloatField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(upload_to='merchandise_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='merchandises', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
