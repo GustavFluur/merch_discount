a0835960af686dc38f81c3fe581e1db4a0ea905e
GustavFluur <gustavfluur@hotmail.com>
2024-01-20 16:37:20 +0000

    added some prep comments for the next step

--- merchandises/forms.py
+++ merchandises/forms.py
@@ -6,4 +6,10 @@ from .models import Merchandise
 class NewMerchandiseForm(forms.ModelForm):
     class Meta:
         model = Merchandise
-        fields = ('category', 'name', 'description', 'price', 'image',) 
+        fields = ('category', 'name', 'description', 'price', 'image',)
+        # widget = {
+            #'category': forms.Select(attrs{
+                #'class': 'kolla bootstrap'
+            #})
+        #}
+     
