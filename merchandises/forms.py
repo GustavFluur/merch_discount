686fd086d85b49dff15b64e3ad17910391de530d
GustavFluur <gustavfluur@hotmail.com>
2024-01-21 18:05:51 +0000

    Added preparation for add merch forms for a later stage

--- merchandises/forms.py
+++ merchandises/forms.py
@@ -2,14 +2,26 @@ from django import forms
 
 from .models import Merchandise
 
-
+# INPUT_CLASSES = 'bootstrap class'
 class NewMerchandiseForm(forms.ModelForm):
     class Meta:
         model = Merchandise
         fields = ('category', 'name', 'description', 'price', 'image',)
         # widget = {
             #'category': forms.Select(attrs{
-                #'class': 'kolla bootstrap'
-            #})
+                #'class': 'kolla bootstrap' INPUT_CLASSES 
+            #}),
+            #'name': forms.TextInput(attrs{
+                #'class': 'kolla bootstrap' INPUT_CLASSES 
+            #}),
+            #'description': forms.Textarea(attrs{
+                #'class': 'kolla bootstrap' INPUT_CLASSES 
+            #}),
+            #'price': forms.TextInput(attrs{
+                #'class': 'kolla bootstrap' INPUT_CLASSES 
+            #}),
+            #'image': forms.FileInput(attrs{
+                #'class': 'kolla bootstrap' INPUT_CLASSES 
+            #}),
         #}
      
