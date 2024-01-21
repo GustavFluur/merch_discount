260ec9120449bd2f3c23a977a0153def4dc9a677
GustavFluur <gustavfluur@hotmail.com>
2024-01-21 18:56:16 +0000

    Added adjustments with its styling for add merch form

--- merchandises/forms.py
+++ merchandises/forms.py
@@ -2,26 +2,26 @@ from django import forms
 
 from .models import Merchandise
 
-# INPUT_CLASSES = 'bootstrap class'
+INPUT_CLASSES = 'col-sm-6 col-md-6 col-lg-4 col-xl-3 text-uppercase justify-content-center'
 class NewMerchandiseForm(forms.ModelForm):
     class Meta:
         model = Merchandise
         fields = ('category', 'name', 'description', 'price', 'image',)
-        # widget = {
-            #'category': forms.Select(attrs{
-                #'class': 'kolla bootstrap' INPUT_CLASSES 
-            #}),
-            #'name': forms.TextInput(attrs{
-                #'class': 'kolla bootstrap' INPUT_CLASSES 
-            #}),
-            #'description': forms.Textarea(attrs{
-                #'class': 'kolla bootstrap' INPUT_CLASSES 
-            #}),
-            #'price': forms.TextInput(attrs{
-                #'class': 'kolla bootstrap' INPUT_CLASSES 
-            #}),
-            #'image': forms.FileInput(attrs{
-                #'class': 'kolla bootstrap' INPUT_CLASSES 
-            #}),
-        #}
+        widget = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+        }
      
